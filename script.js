e1d57532ba439c561ecb4f693923472ed2602547
neeeeerd <clarceliz2006@gmail.com>
2024-12-02 14:05:02 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Toggle dropdown visibility
+function toggleDropdown() {
+    const profileSection = document.querySelector('.profile-section');
+    profileSection.classList.toggle('show-dropdown');
+  }
+  
